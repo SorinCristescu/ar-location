b02e8230e1308857a8456e2159ebdc6a78fbba99
SorinCristescu <sorin.t.cristescu@gmail.com>
2020-07-24 16:40:29 +0200

    updates

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function staticLoadPlaces() {
       name: 'AR location based',
       location: {
         // decomment the following and add coordinates:
-        lat: 55.649142,
-        lng: 12.554941,
+        lat: 55.649084,
+        lng: 12.555048,
       },
     },
   ];
